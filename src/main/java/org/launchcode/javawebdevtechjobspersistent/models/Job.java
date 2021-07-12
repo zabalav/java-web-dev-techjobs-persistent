7e8564bab0a281e888372a29ee28cdba3afabe11
zabalav <vincent_zabala@yahoo.com>
2021-07-11 20:27:06 -0500

    Updating HomeController Part 3

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Job.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Job.java
@@ -1,23 +1,26 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-public class Job{
-
-    @Id
-    @GeneratedValue
-    private int id;
+public class Job extends AbstractEntity{
 
     private String name;
 
-    private String employer;
+    @ManyToOne
+    private Employer employer;
     private String skills;
+
+    @OneToMany(mappedBy = "") //"Job" or "JobData"
+    @JoinColumn
+    private final List<Job> jobs = new ArrayList<>();
 
     public Job() {
     }
 
-    public Job(String anEmployer, String someSkills) {
+    public Job(Employer anEmployer, String someSkills) {
         super();
         this.employer = anEmployer;
         this.skills = someSkills;
@@ -25,19 +28,11 @@ public class Job{
 
     // Getters and setters.
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmployer() {
+    public Employer getEmployer() {
         return employer;
     }
 
-    public void setEmployer(String employer) {
+    public void setEmployer(Employer employer) {
         this.employer = employer;
     }
 
