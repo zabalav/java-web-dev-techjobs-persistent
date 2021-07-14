fbb5aea878a340800d6530fc67b82902d7bab90a
zabalav <vincent_zabala@yahoo.com>
2021-07-14 11:04:28 -0500

    Moving on to Part 4

--- queries.sql
+++ queries.sql
@@ -9,6 +9,9 @@ skills varchar(255)
 ## Part 2: Test it with SQL - write a query to list the names of the employers in St. Louis City.
 SELECT * FROM techjobs.employer WHERE location LIKE 'St Louis City%';
 
-## Part 3: Test it with SQL
-
-## Part 4: Test it with SQL
+## Part 3: Test it with SQL - write the SQL statement to remove the job table.
+DELETE job FROM techjobs;
+## Part 4: Test it with SQL - write a query to return a list of the names and
+descriptions of all skills that are attached to jobs in alphabetical order.
+If a skill does not have a job listed, it should not be included in the results
+of this query.
